b09272e9b0685c070ccf974bc4c2d4a893eaa75f
Nanda-25 <113986489+Nanda-25@users.noreply.github.com>
2023-03-01 19:04:31 +0800

    update

---
+++ src/pages/ProjectPage.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+function ProjectPage() {
+    //State
+const [projectData, setProjectData] = useState({pledges: []})
+//Hooks
+
+const { id }  = useParams();
+
+//Effects
+useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}projects/${id}`)
+        .then((results) => {
+                setProjectData(data);
+        });
+}, []);
+
+    return (
+        <div>
+            <h2>{projectData.title}</h2>
+            <h3>Created at: {projectData.date_created}</h3>
+            <h3>{`Status: ${projectData.is_open}`}</h3>
+            <h3>Pledges:</h3>
+            <ul>
+                {projectData.pledges.map((pledgeData, key) => {
+                    return (
+                        <li>
+                            {pledgeData.amount} from {pledgeData.supporter}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+}
+
+export default ProjectPage;
